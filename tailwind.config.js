d1ee1fcb96e06289ab340396f9b42791f5907b08
tikosaghinadze <118922621+tikosaghinadze@users.noreply.github.com>
2025-03-19 03:54:30 -0700

    add custume colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#F64A4A",
+        orange: "#FB7C58",
+        yellow: "#F8CD65",
+        neonGreen: "#A4FFAF",
+        almostWhite: "#E6E5EA",
+        grey: "#817D92",
+        darkGrey: "#24232C",
+        veryDarkGrey: "#18171F",
+      },
+    },
   },
   plugins: [],
 };
